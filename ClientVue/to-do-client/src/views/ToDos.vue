be1a8b40a1d6063241938f9f1f3aa1644a41b1c3
Piotrek <kurdapiotr99@icloud.com>
2022-11-10 12:17:07 +0100

    Vue client fetching todos, cors fix

--- ClientVue/to-do-client/src/views/ToDos.vue
+++ ClientVue/to-do-client/src/views/ToDos.vue
@@ -1,12 +1,67 @@
 <template>
     <HeaderComponent />
+    <div v-if="showAlert" :class="alertType" role="alert">
+        {{alertMessage}}
+    </div>
+    <div v-if="loading" class="d-flex justify-content-center">
+        <div class="spinner-border" style="width: 3rem; height: 3rem; margin-top: 3rem;" role="status">
+        <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+    <div v-if="!loading" class="data">
+        <pre>{{todosData}}</pre>
+    </div>
 </template>
 
 <script>
 import HeaderComponent from '../components/Header.vue'
 export default {
     name: 'ToDosComponent',
-    components: { HeaderComponent }
+    components: { HeaderComponent },
+    data() {
+        return {
+            todosData: [],
+            loading: false,
+            isAddAlertVisible: false,
+            isAddViewVisible: false,
+            alertMessage: '',
+            alertType: '',
+            showAlert: false
+        }
+    },
+    mounted() {
+        this.getToDos();
+    },
+    methods: {
+        handleAddToDoClicked(value) {
+            this.isAddViewVisible = value;
+        },
+        displayAlert(message, type) {
+            this.alertMessage = message;
+            this.alertType = type;
+            this.showAlert = true;
+            setTimeout(() => {
+            this.showAlert = false
+            }, 2000);
+        },
+        async getToDos() {
+            this.loading = true;
+            await fetch("http://localhost:8080/todo")
+            .catch(() => {
+                this.displayAlert('Wystąpił błąd!', 'alert alert-danger');
+                this.loading = false;
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                if(data.error) {
+                    this.displayAlert('Wystąpił błąd!', 'alert alert-danger');
+                } else {
+                    this.todosData = data;
+                }
+            })
+            this.loading = false;
+        }
+    }
 }
 </script>
 
